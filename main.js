09124a3427e62ff6c5c58c34b9950b15ffa4b004
coding-learn11 <82395877+coding-learn11@users.noreply.github.com>
2024-02-14 18:54:33 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
+song1_status = "";
+song2_status = "";
+
 
 function preLoad() {
     song1 = loadSound("From The Start - Laufey.mp3");
@@ -28,6 +33,7 @@ function modelLoaded() {
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
@@ -40,4 +46,28 @@ function gotPoses(results) {
 
 function draw() {
     image(video, 0, 0, 600, 500);
+    fill("FF0000");
+    stroke("FF0000");
+    song1_status = song1.isPlaying();
+    console.log(song1_status);
+    song2_status = song2.isPlaying();
+    console.log(song2_status);
+    if(scoreLeftWrist > 0.2) {
+        circle(leftWristX, leftWristY, 20);
+        song2.stop();
+        
+        if(song1_status == false) {
+            song1.play();
+            document.getElementById("song_name").innerHTML = "Song Name: From the Start by Laufey";
+        }
+    }
+    if(scoreRightWrist > 0.2) {
+        circle(rightWristX, rightWristY, 20)
+        song1.stop();
+        if(song2_status == false) {
+            song2.play();
+            document.getElementById("song_name").innerHTML= "Song Name: Feels Like by Gracie Abrams";  
+        }
+    }
+
 }
